c0a7a17997bba456261c1c11b17493860dbdaf03
Rajavel <143553764+RajavelRajendiran@users.noreply.github.com>
2023-09-21 17:35:38 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-x=1#x is a int
-y="raja"#y is a string
+a=1#x is a int
+b="raja"#y is a string
+fruits = ["apple","orange","cherry"]
+x, y, z = fruits
+print(a)
+print(b)
 print(x)
 print(y)
+print(z)
