a981a2db7d49b292bb2fd5b254205266cb39a054
Rajavel <143553764+RajavelRajendiran@users.noreply.github.com>
2023-09-22 07:44:51 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,32 @@
-print("Hello CodeSandbox!")
-print("hi")
+a=5 # int datatype
+b="hi" #str datatype
+c=1.5 # float datatype
+d=["apple","orange","banana"] # list datatype
+e=("apple","orange","banana") # tuple datatype
+f=range(5)# range datatype
+g={"Name":"Raja","age":"26"}# dict datatype
+h={"apple","orange","banana"} # set datatype
+i=({"apple","oran"})# frosenset datatype
+j = True  # bool datatype
+k=b"hello"# byte datatype
+l = bytearray(5)# bytearray datatype
+m = memoryview(bytes(5))# memoryview datatype
+n = None# NoneType datatype
+print(a,b,c,d,e,f,g,h,i,j,k,l,m,n)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(i))
+print(type(j))
+print(type(k))
+print(type(l))
+print(type(m))
+print(type(n))
+
+
+
