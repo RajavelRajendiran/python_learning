6622ffc8e8c1a9129774c2a10ff7c32d421957a3
Rajavel <143553764+RajavelRajendiran@users.noreply.github.com>
2023-09-22 12:05:48 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-print("Hello CodeSandbox!")
-print("hi")
+myvar = "John"
+my_var = "rio"
+_my_var = "jack"
+myVar = "battery"
+MYVAR = "John"
+myvar2 = "John"
+x = 5
+y = "John"
+print(x , y)
+a = 5
+b = "raja"
+c = "Python "
+d = "is "
+e = "awesome"
+print(c + d + e)
+print(a, b)
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
+
