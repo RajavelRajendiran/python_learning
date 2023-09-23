30dc065004d7835ffb12e422f699e33ae160133b
Rajavel <143553764+RajavelRajendiran@users.noreply.github.com>
2023-09-23 16:10:12 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,36 @@
-print("Hello CodeSandbox!")
-print("hi")
+x = 2
+y = 5
+"""Arithmetic operators are used with numeric values 
+   to perform common mathematical operations:"""
+print(x + y)#Addition
+print(x - y)#Subtraction
+print(x * y)#Multiplication
+print(x / y)#Division
+print(x ** y)#Exponentiation
+print(x //y)#Floor division
+print(x % y)#Modulus
+"""Assignment operators are used to assign values to variables:"""
+[a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r]=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+a += 3#x = x + 3	
+b -= 3#	x = x - 3	
+c *= 3#	x = x * 3	
+d /= 3#	x = x / 3	
+e %= 3#	x = x % 3	
+f //= 3#	x = x // 3	
+g **= 3#	x = x ** 3	
+h &= 3#	x = x & 3	
+i |= 3#	x = x | 3	
+j ^= 3#	x = x ^ 3	
+k >>= 3#	x = x >> 3	
+l <<= 3#	x = x << 3	
+"""Comparison operators are used to compare two values:"""
+m == y#Equal	
+n != y#(!=)Not equal		
+o>y#(>)Greater than	
+p<y#(<)Less than	x < y	
+q>=y#(>=)Greater than or equal to	
+r<=y#Less than or equal to	x <= y	
+print(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,)
+
+
+	
