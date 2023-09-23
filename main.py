36916aba854344bc72eacfe9c9f8f2c15339deae
Rajavel <143553764+RajavelRajendiran@users.noreply.github.com>
2023-09-23 11:36:16 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,32 @@
-print("Hello CodeSandbox!")
-print("hi")
+print(10 > 9)
+print(10 == 9)
+print(10 < 9) 
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a") 
+print(bool("Hello"))
+print(bool(15))
+x = "Hello"
+y = 15
+print(bool(x))
+print(bool(y))
+print(bool("abc"))
+print(bool(123))
+print(bool(["apple", "cherry", "banana"]))
+def myFunction() :
+  return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!") 
+print(bool(False))
+print(bool(None))
+print(bool(""))
+print(bool(()))
+print(bool([]))
+print(bool({}) )
