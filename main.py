6367c5a0a67eadd74f30916615499e6b8b443568
Rajavel <143553764+RajavelRajendiran@users.noreply.github.com>
2023-09-23 10:29:45 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-print("Hello CodeSandbox!")
-print("hi")
+txt = " The best things in life are free! "
+print(txt[:5])
+print("free" in txt)
+print(txt.replace("best","worst"))
+print(txt.strip())
+print(txt.upper())
+print(txt.split(","))
+for x in txt:
+    print(x)
+if "free" in txt:
+    print("Yes, 'free' is present.")
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+age=25
+month=2
+year=5
+d="iam raja and my age is {0}\n with{2}years,{1}months of exp"
+print(d.format(age,month,year))
+    
