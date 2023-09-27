c97b788adaeedc19be2b25a7a6bc1009dff86722
Rajavel <143553764+RajavelRajendiran@users.noreply.github.com>
2023-09-27 11:41:07 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-print("Hello CodeSandbox!")
-print("hi")
+a = 330
+b = 330
+print("A") if a > b else print("=") if a == b else print("B")
+c=2
+d=5
+if c>d:
+    print("c if greater than d")
+elif c==d:
+    print("c=d")
+else:
+    print("d is greater than c")
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+for x in range(6):
+  if x == 3: break
+  print(x)
+else:
+  print("Finally finished!")
