704fd80dc5569ee12c04a76c4b15400f60cce28e
Rajavel <143553764+RajavelRajendiran@users.noreply.github.com>
2023-09-27 18:35:29 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-print("Hello CodeSandbox!")
-print("hi")
+def tri_recurtion(k):
+  if(k>0):
+    result = k + tri_recurtion(k-1)
+    print(result)
+  else:
+    result=0
+  return result
+print("\n\nRecurtion Example Results")
+tri_recurtion(6)
+def my_function(food):
+  for x in food:
+    print(x)
+
+fruits = ["apple", "banana", "cherry"]
+
+my_function(fruits)
