ef9a84d2d27d9a8410a58b0a316a437eb3508e07
Rajavel <143553764+RajavelRajendiran@users.noreply.github.com>
2023-09-28 19:19:24 +0000

    Update CANoutput.json and main.py

--- main.py
+++ main.py
@@ -10,14 +10,23 @@ with open(input_file_name, "r") as input_file:
 
 # Initialize an empty list to store the extracted data
 extracted_data = []
+# Initialize variables to hold the message ID and message name
+message = None
+sender_node = None
 
 # Iterate through the lines and extract the required from lines starting with "BO_"
 for line in lines:
     if line.startswith("BO_"):
-        extracted_text = line
-        a = extracted_text.rindex(":")
-        updated_text = line.strip()[3:a]  # Extract the required data
-        extracted_data.append(updated_text)
+ # If there's already data from a previous "BO_" line, append it to the extracted data
+     if message and sender_node:
+       extracted_data.append({"message": message, "sender_node": sender_node})
+     extracted_text = line
+     a = extracted_text.rindex(":")
+     b=(2+extracted_text.rindex("8 "))
+     message = line.strip()[3:a]  # Extract required data after "BO_"
+     sender_node = line.strip()[b:]  # Extract required data after "BO_"
+if message and sender_node:
+    extracted_data.append({"message": message, "sender_node": sender_node})
 # Convert the extracted lines to a JSON structure and write it to the output JSON file
 with open(output_file_name, 'w') as output_file:
     json.dump(extracted_data, output_file, indent=2)
