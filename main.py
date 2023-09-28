9b6f3009cb201db78267bd0d8da2803312c068e2
Rajavel <143553764+RajavelRajendiran@users.noreply.github.com>
2023-09-28 09:53:40 +0000

    Update 3 files

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
-print("Hello CodeSandbox!")
-print("hi")
+# Specify the input and output file names
+input_file_name = "ChassisCAN1.dbc"
+output_file_name = "CANoutput.dbc"
+
+# Open the input file for reading
+with open(input_file_name, "r") as input_file:
+    # Read all lines from the input file
+    lines = input_file.readlines()
+
+# Initialize an empty list to store the extracted data
+extracted_data = []
+
+# Iterate through the lines and extract the first 17 characters from lines starting with "BO_"
+for line in lines:
+    if line.startswith("BO_"):
+        extracted_text = line.strip()[3:20]  # Extract the first 17 characters after "BO_"
+        extracted_data.append(extracted_text)
+
+# Open the output file for writing and write the extracted data to it
+with open(output_file_name, "w") as output_file:
+    for data in extracted_data:
+        output_file.write(data + "\n")
+
+# Print a confirmation message
+print(f"Extracted and wrote to {output_file_name}:\n{extracted_data}")
