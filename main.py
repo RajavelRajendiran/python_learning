435bfb5feb8911d2710daf5f512f55903fbab080
Rajavel <143553764+RajavelRajendiran@users.noreply.github.com>
2023-10-01 11:05:53 +0000

    Update CANoutput.json and main.py

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import json
 # Specify the input and output file names
 input_file_name = "ChassisCAN1.dbc"
 output_file_name = "CANoutput.json"
-# Initialize empty lists to store the extracted data
+
+# Initialize a list to store the extracted data
 extracted_data = []
-# Initialize variables to hold message, sender_node, and signal information
+
+# Initialize a variable to hold the current message information
 current_bo = None
-current_sg = None
 
 # Open the input file for reading
 with open(input_file_name, "r") as input_file:
@@ -15,52 +16,41 @@ with open(input_file_name, "r") as input_file:
         # Remove leading and trailing whitespace
         line = line.strip()
 
-        # Skip lines that do not start with "BO_", "SG_", or other relevant prefixes
-        if not line.startswith(("BO_", "SG_")):
-            continue
+        # Process only lines that start with "BO_" or "SG_"
+        if line.startswith(("BO_", "SG_")):
+            if line.startswith("BO_"):
+                # Save the previous message if it exists
+                if current_bo is not None:
+                    extracted_data.append(current_bo)
 
-        if line.startswith("BO_"):
-            # If a new BO_ line is encountered, add the previous BO_ dictionary to the data list
-            if current_bo is not None:
-                if current_sg is not None:
-                    current_bo["SG"].append(current_sg)
-                extracted_data.append(current_bo)
-            bo_parts = line.strip().split()
-            if len(bo_parts) >= 2:
-                # Extract the current BO information
-                current_bo = {
-                    "BO": line.strip(),
-                    "message": bo_parts[2],
-                    "sender_node": bo_parts[5],
-                    "SG": []
-                }
-            else:
-                current_bo = None
-        elif line.startswith("SG_"):
-            if current_bo is not None:
-                sg_parts = line.strip().split()
-                if len(sg_parts) >= 2:
-                    # Extract the SG information and add it to the current BO dictionary's SG list
-                    current_sg = {
-                        "signal":line.strip(),
-                        "signal_name": sg_parts[1],
-                        "bit_position":sg_parts[3].split('|')[0].strip(),
-                        "length_of_bit":sg_parts[3].split('|')[1].strip("@0+"),
-                        "factor":sg_parts[4].strip("()").split(",")[0],
-                        "offset":sg_parts[4].strip("()").split(",")[1],
-                        "min_value":sg_parts[5].strip("[]").split("|")[0],
-                        "max_value":sg_parts[5].strip("[]").split("|")[1],
-                        "unit":sg_parts[6].strip("\"\""),
-                        "receiver_node":sg_parts[7]
-
-
+                # Parse BO_ line to extract message information
+                bo_parts = line.split()
+                if len(bo_parts) >= 6:
+                    current_bo = {
+                        "BO": line,
+                        "message": bo_parts[2],
+                        "sender_node": bo_parts[5],
+                        "SG": []
                     }
-                    current_bo["SG"].append(current_sg)
+            elif current_bo is not None:
+                # Parse SG_ line to extract signal information and add it to the current message's "SG" list
+                sg_parts = line.split()
+                if len(sg_parts) >= 8:
+                    current_bo["SG"].append({
+                        "signal": line,
+                        "signal_name": sg_parts[1],
+                        "bit_position": sg_parts[3].split('|')[0],
+                        "length_of_bit": sg_parts[3].split('|')[1].split('@')[0],
+                        "factor": sg_parts[4].split(',')[0].strip('()'),
+                        "offset": sg_parts[4].split(',')[1].strip('()'),
+                        "min_value": sg_parts[5].split('|')[0].strip('[]'),
+                        "max_value": sg_parts[5].split('|')[1].strip('[]'),
+                        "unit": sg_parts[6].strip('"'),
+                        "receiver_node": sg_parts[7]
+                    })
 
 # Add the last extracted BO_ (if any) to the extracted_data list
 if current_bo is not None:
-    if current_sg is not None:
-        current_bo["SG"].append(current_sg)
     extracted_data.append(current_bo)
 
 # Convert the extracted data to a JSON structure and write it to the output JSON file
